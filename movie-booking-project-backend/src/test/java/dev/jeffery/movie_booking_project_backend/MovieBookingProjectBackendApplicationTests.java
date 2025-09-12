04f2ce5950ea1d7d0027b32bf1029894e0d1d1f5
jfelshaw <jef57701@uga.edu>
2025-09-12 15:09:05 -0400

    Add files via upload

---
+++ movie-booking-project-backend/src/test/java/dev/jeffery/movie_booking_project_backend/MovieBookingProjectBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.jeffery.movie_booking_project_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieBookingProjectBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
