ea7a5423896eabafd5edbb9365944ef721f5d194
Griffin Robinson <78376643+GriffRob1@users.noreply.github.com>
2025-09-23 22:47:54 -0400

    removed some http calls in fetch_calls.js

--- movie-booking-project-frontend/fetch_calls.js
+++ movie-booking-project-frontend/fetch_calls.js
@@ -18,38 +18,14 @@ getFilteredMovies(null, "drama") //not filter by title, either use whitespace or
 getFilteredMovies("the godfather", "Any")   //use 'Any' to not filter by genre
 
 
-function getFilteredMoviesWithDate(title, genre, date) { //date is in form mm/dd/yyyy
-    let filteredMovies = []
-    fetch(`http://localhost:8080/movies/${title}/${genre}/${date}`)
-        .then((response) => response.json())
-        .then((data) => {
-            filteredMovies = data
-            console.log(filteredMovies)
-        })
-        .catch((err) => console.error('Error:', err))
-    return filteredMovies
-}
 
-function getRunningMovies() { //under the "now showing" part of the web page. gets movies with running: true
-    let runningMovies = []
-    fetch(`http://localhost:8080/movies/running`)
-        .then((response) => response.json())
-        .then((data) => {
-            runningMovies = data
-            console.log(runningMovies)
-        })
-        .catch((err) => console.error('Error:', err))
-    return runningMovies
-}
-
-function getComingSoonMovies() { //under the "coming soon" part of the web page. gets movies with running: false
-    let comingSoonMovies = []
-    fetch(`http://localhost:8080/movies/coming-soon`)
-        .then((response) => response.json())
-        .then((data) => {
-            comingSoonMovies = data
-            console.log(comingSoonMovies)
-        })
-        .catch((err) => console.error('Error:', err))
-    return comingSoonMovies
-}
+allMovies = getFilteredMovies(null, "Any")
+comingSoonMovies = []
+nowPlayingMovies = []
+allMovies.forEach(movie => {
+    if (movie.running) {
+        nowPlayingMovies.append(movie)
+    } else {
+        comingSoonMovies.append(movie)
+    }
+})
