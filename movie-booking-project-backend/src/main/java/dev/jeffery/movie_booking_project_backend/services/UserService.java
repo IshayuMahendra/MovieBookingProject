e3ad8de3f685aa1fedf0a60dff873e84c92329e4
Chewwi7 <jeffery.felshaw@icloud.com>
2025-10-27 21:29:28 -0400

    small change

--- movie-booking-project-backend/src/main/java/dev/jeffery/movie_booking_project_backend/services/UserService.java
+++ movie-booking-project-backend/src/main/java/dev/jeffery/movie_booking_project_backend/services/UserService.java
@@ -84,18 +84,12 @@ public class UserService {
         if (updatedInfoTemplate.getLastName() != null && !updatedInfoTemplate.getLastName().isBlank()) {
             user.setLastName(updatedInfoTemplate.getLastName());
         }
-        if (updatedInfoTemplate.getStreet() != null && !updatedInfoTemplate.getStreet().isBlank()) {
-            user.setStreet(updatedInfoTemplate.getStreet());
-        }
-        if (updatedInfoTemplate.getCity() != null && !updatedInfoTemplate.getCity().isBlank()) {
-            user.setCity(updatedInfoTemplate.getCity());
-        }
-        if (updatedInfoTemplate.getState() != null && !updatedInfoTemplate.getState().isBlank()) {
-            user.setState(updatedInfoTemplate.getState());
-        }
-        if (updatedInfoTemplate.getZipCode() != null && !updatedInfoTemplate.getZipCode().isBlank()) {
-            user.setZipCode(updatedInfoTemplate.getZipCode());
-        }
+
+        user.setStreet(updatedInfoTemplate.getStreet());
+        user.setCity(updatedInfoTemplate.getCity());
+        user.setState(updatedInfoTemplate.getState());
+        user.setZipCode(updatedInfoTemplate.getZipCode());
+
         user.setPromotions(updatedInfoTemplate.getPromotions());
         userRepository.save(user);
         try {
